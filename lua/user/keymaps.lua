98f618d5f4e116279fa660c4a69d1f60c8722071
lucky-vers <vermalucky2004@gmail.com>
2022-06-13 06:13:00 +0530

    Replace tabs with spaces

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,131 +10,123 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Mode indicators
-	-- Normal Mode       = n
-	-- Insert Mode       = i
-	-- Visual Mode       = v
-	-- Visual Block Mode = x
-	-- Terminal Mode     = t
-	-- Command Mode      = c
+  -- Normal Mode       = n
+  -- Insert Mode       = i
+  -- Visual Mode       = v
+  -- Visual Block Mode = x
+  -- Terminal Mode     = t
+  -- Command Mode      = c
 
 -- NORMAL --
 
-	-- Insert keymaps
- 	map("n", "<leader>N", "o<ESC>0C<TAB>map(\"n\", \"<++>\", \"<++>\", opts)<ESC>0", opts)	-- Normal Mode Keymap
-	map("n", "<leader>I", "o<ESC>0C<TAB>map(\"i\", \"<++>\", \"<++>\", opts)<ESC>0", opts)	-- Insert Mode Keymap
-	map("n", "<leader>V", "o<ESC>0C<TAB>map(\"v\", \"<++>\", \"<++>\", opts)<ESC>0", opts)	-- Visual Mode Keymap
-	map("n", "<leader>X", "o<ESC>0C<TAB>map(\"x\", \"<++>\", \"<++>\", opts)<ESC>0", opts)	-- Visual Block Mode Keymap
-	map("n", "<leader>T", "o<ESC>0C<TAB>map(\"t\", \"<++>\", \"<++>\", opts)<ESC>0", opts)	-- Terminal Mode Keymap
-	map("n", "<leader>C", "o<ESC>0C<TAB>map(\"c\", \"<++>\", \"<++>\", opts)<ESC>0", opts)	-- Command Mode Keymap
+  -- Better window navigation
+  map("n", "<C-h>", "<C-w>h", opts) -- Move left
+  map("n", "<C-j>", "<C-w>j", opts) -- Move down
+  map("n", "<C-k>", "<C-w>k", opts) -- Move up
+  map("n", "<C-l>", "<C-w>l", opts) -- Move right
 
-	-- Better window navigation
-	map("n", "<C-h>", "<C-w>h", opts) -- Move left
-	map("n", "<C-j>", "<C-w>j", opts) -- Move down
-	map("n", "<C-k>", "<C-w>k", opts) -- Move up
-	map("n", "<C-l>", "<C-w>l", opts) -- Move right
+  -- Scroll pages
+  map("n", "<C-l>", "<C-u>", opts)
+  map("n", "<C-h>", "<C-d>", opts)
 
-	-- Scroll pages
-	map("n", "<C-l>", "<C-u>", opts)
-	map("n", "<C-h>", "<C-d>", opts)
+  -- Resize with arrows
+   map("n", "<C-Up>",   ":resize +2<CR>",           opts) -- Vertically resize up by 2
+  map("n",  "<C-Down>",  ":resize -2<CR>",          opts) -- Vertically resize down by 2
+  map("n",  "<C-Left>",  ":vertical resize -2<CR>", opts) -- Vertically resize left by 2
+  map("n",  "<C-Right>", ":vertical resize +2<CR>", opts) -- Vertically resize right by 2
 
-	-- Resize with arrows
- 	map("n", "<C-Up>",    ":resize +2<CR>",          opts) -- Vertically resize up by 2
-	map("n", "<C-Down>",  ":resize -2<CR>",          opts) -- Vertically resize down by 2
-	map("n", "<C-Left>",  ":vertical resize -2<CR>", opts) -- Vertically resize left by 2
-	map("n", "<C-Right>", ":vertical resize +2<CR>", opts) -- Vertically resize right by 2
+  -- Navigate buffers
+  map("n", "<S-l>", ":bnext<CR>",     opts) -- Go to next buffer
+  map("n", "<S-h>", ":bprevious<CR>", opts) -- Go to previous buffer
 
-	-- Navigate buffers
-	map("n", "<S-l>", ":bnext<CR>",     opts) -- Go to next buffer
-	map("n", "<S-h>", ":bprevious<CR>", opts) -- Go to previous buffer
+  -- Enter visual block mode
+  map("n", "<leader>v", "<C-v>", opts)
 
-	-- Enter visual block mode
-	map("n", "<leader>v", "<C-v>", opts)
+  -- Save the file
+  map("n",  "s", ":%s/\\s\\+$//e<CR>:w<CR>",  opts) -- Save
+  map("n",  "S", ":%s/\\s\\+$//e<CR>ZZ",      opts) -- Save & Quit
+  map("n", "QQ", ":%s/\\s\\+$//e<CR>:q!<CR>", opts) -- Quit without saving
 
-	-- Save the file
-	map("n",  "s", ":%s/\\s\\+$//e<CR>:w<CR>",  opts) -- Save
-	map("n",  "S", ":%s/\\s\\+$//e<CR>ZZ",      opts) -- Save & Quit
-	map("n", "QQ", ":%s/\\s\\+$//e<CR>:q!<CR>", opts) -- Quit without saving
+  -- Go to line extremes
+  map("n", "<TAB>", "0", opts) -- Go to begining
+  map("n", "<S-TAB>", "$", opts) -- Go to end
 
-	-- Go to line extremes
-	map("n", "<TAB>", "0", opts) -- Go to begining
-	map("n", "<S-TAB>", "$", opts) -- Go to end
+  -- Select all text
+  map("n", "<C-a>", "ggvG2", opts)
 
-	-- Select all text
-	map("n", "<C-a>", "ggvG2", opts)
+  -- Move to <++>
+  map("n", "<leader><leader>", "/<++><CR>ca<", opts)
 
-	-- Move to <++>
-	map("n", "<leader><leader>", "/<++><CR>ca<", opts)
+  -- Search and Replace
+  map("n", "<leader>r", ":%s//gi<left><left><left>", opts)
 
-	-- Search and Replace
-	map("n", "<leader>r", ":%s//gi<left><left><left>", opts)
+  -- Global
+  map("n", "<leader>g", ":g/", opts)
 
-	-- Global
-	map("n", "<leader>g", ":g/", opts)
+  -- Remove search highlight
+  map("n", "<ESC><ESC>", "/<ESC>:noh<CR>", opts)
 
-	-- Remove search highlight
-	map("n", "<ESC><ESC>", "/<ESC>:noh<CR>", opts)
+  -- Increment numbers
+  map("n", "=", "<C-a>", opts) -- Increase value
+  map("n", "-", "<C-x>", opts) -- Decrease value
 
-	-- Increment numbers
-	map("n", "=", "<C-a>", opts) -- Increase value
-	map("n", "-", "<C-x>", opts) -- Decrease value
+  -- Move to middle while scrolling to bottom
+  map("n", "G", "Gzz", opts)
 
-	-- Move to middle while scrolling to bottom
-	map("n", "G", "Gzz", opts)
+  -- Add a space
+  map("n", ",", "i<space><ESC>", opts)
 
-	-- Add a space
-	map("n", ",", "i<space><ESC>", opts)
+  -- Scroll through truncated lines
+  map("n", "<A-J>", "gj", opts) -- Scroll down
+  map("n", "<A-K>", "gk", opts) -- Scroll up
 
-	-- Scroll through truncated lines
-	map("n", "<A-J>", "gj", opts) -- Scroll down
-	map("n", "<A-K>", "gk", opts) -- Scroll up
-
-	-- Move text up and down
-	map("n", "<A-j>", ":m .+1<CR>", opts)
-	map("n", "<A-k>", ":m .-2<CR>", opts)
+  -- Move text up and down
+  map("n", "<A-j>", ":m .+1<CR>", opts)
+  map("n", "<A-k>", ":m .-2<CR>", opts)
 
 -- INSERT --
 
-	-- Press jj fast to enter visual mode
-	map("i", "jj", "<ESC>", opts)
+  -- Press jj fast to enter visual mode
+  map("i", "jj", "<ESC>", opts)
 
-	-- Autocomplete brackets and quotes
-	map("i", "\"", "\"\"<ESC>i", opts) -- Double quotes
-	map("i", "\'", "\'\'<ESC>i", opts) -- Single quotes
-	map("i", "(",  "()<ESC>i",   opts) -- Circle brackets
-	map("i", "[",  "[]<ESC>i",   opts) -- Square brackets
-	map("i", "{",  "{}<ESC>i",   opts) -- Curly brackets
+  -- Autocomplete brackets and quotes
+  map("i", "\"", "\"\"<ESC>i", opts) -- Double quotes
+  map("i", "\'", "\'\'<ESC>i", opts) -- Single quotes
+  map("i", "(",  "()<ESC>i",   opts) -- Circle brackets
+  map("i", "[",  "[]<ESC>i",   opts) -- Square brackets
+  map("i", "{",  "{}<ESC>i",   opts) -- Curly brackets
 
 -- VISUAL --
 
-	-- Stay in indent mode
-	map("v", "<", "<gv", opts) -- Indent left
-	map("v", ">", ">gv", opts) -- Indent right
+  -- Stay in indent mode
+  map("v", "<", "<gv", opts) -- Indent left
+  map("v", ">", ">gv", opts) -- Indent right
 
-	-- Add brackets and quotes to selection
-	map("v", "\"", "\"-c\"\"<ESC>\"-P", opts)
-	map("v", "\'", "\"-c\'\'<ESC>\"-P", opts)
-	map("v", "{",   "\"-c{}<ESC>\"-P",  opts)
-	map("v", "(",   "\"-c()<ESC>\"-P",  opts)
-	map("v", "[",   "\"-c[]<ESC>\"-P",  opts)
+  -- Add brackets and quotes to selection
+  map("v", "\"", "\"-c\"\"<ESC>\"-P", opts)
+  map("v", "\'", "\"-c\'\'<ESC>\"-P", opts)
+  map("v", "{",   "\"-c{}<ESC>\"-P",  opts)
+  map("v", "(",   "\"-c()<ESC>\"-P",  opts)
+  map("v", "[",   "\"-c[]<ESC>\"-P",  opts)
 
-	-- Move text up and down
-	map("v", "<A-j>", ":m .+1<CR>==", opts)
-	map("v", "<A-k>", ":m .-2<CR>==", opts)
+  -- Move text up and down
+  map("v", "<A-j>", ":m .+1<CR>==", opts)
+  map("v", "<A-k>", ":m .-2<CR>==", opts)
 
-	-- Go to line extremes
-	map("v", "<TAB>", "0", opts) -- Go to begining
-	map("v", "<S-TAB>", "$", opts) -- Go to end
+  -- Go to line extremes
+  map("v", "<TAB>", "0", opts) -- Go to begining
+  map("v", "<S-TAB>", "$", opts) -- Go to end
 
 -- VISUAL BLOCK --
 
-	-- Move text
-	map("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-	map("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+  -- Move text
+  map("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+  map("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- TERMINAL --
 
-	-- Better terminal navigation
-	map("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-	map("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-	map("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-	map("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+  -- Better terminal navigation
+  map("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+  map("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+  map("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+  map("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,85 +3,85 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system {
-	  "git",
-	  "clone",
-	  "--depth",
-	  "1",
-	  "https://github.com/wbthomason/packer.nvim",
-	  install_path,
-	}
-	print "Installing packer close and reopen Neovim..."
-	vim.cmd [[packadd packer.nvim]]
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
-	augroup packer_user_config
-	  autocmd!
-	  autocmd BufWritePost plugins.lua source <afile> | PackerSync
-	augroup end
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
 ]]
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
 packer.init {
-	display = {
-	  open_fn = function()
-	    return require("packer.util").float { border = "rounded" }
-	  end,
-	},
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
 
-	-- General
-	use "wbthomason/packer.nvim"           -- Have packer manage itself
-	use "nvim-lua/popup.nvim"              -- An implementation of the Popup API from vim in Neovim
-	use "nvim-lua/plenary.nvim"            -- Useful lua functions used ny lots of plugins
-	use "vim-airline/vim-airline"          -- A simple status bar
-	use "vim-airline/vim-airline-themes"   -- Themes for the status bar
-	use "psliwka/vim-smoothie"             -- Vim smooth scrolling
+  -- General
+  use "wbthomason/packer.nvim"           -- Have packer manage itself
+  use "nvim-lua/popup.nvim"              -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim"            -- Useful lua functions used ny lots of plugins
+  use "vim-airline/vim-airline"          -- A simple status bar
+  use "vim-airline/vim-airline-themes"   -- Themes for the status bar
+  use "psliwka/vim-smoothie"             -- Vim smooth scrolling
 
-	-- Color Schemes
-	use "morhetz/gruvbox"                  -- Gruvbox
-	use "catppuccin/nvim"                  -- Catpuccin
-	use "altercation/vim-colors-solarized" -- Solarized
-	use "LunarVim/Colorschemes"            -- LunarVim color schemes
-	use "folke/tokyonight.nvim"            -- Tokyo Night
-	use "dracula/vim"                      -- Dracula
-	use "flazz/vim-colorschemes"           -- A package of multiple color schemes
+  -- Color Schemes
+  use "morhetz/gruvbox"                  -- Gruvbox
+  use "catppuccin/nvim"                  -- Catpuccin
+  use "altercation/vim-colors-solarized" -- Solarized
+  use "LunarVim/Colorschemes"            -- LunarVim color schemes
+  use "folke/tokyonight.nvim"            -- Tokyo Night
+  use "dracula/vim"                      -- Dracula
+  use "flazz/vim-colorschemes"           -- A package of multiple color schemes
 
-	-- CMP
-	use "hrsh7th/nvim-cmp"         -- Completion plugin
-	use "hrsh7th/cmp-buffer"       -- Buffer Completions
-	use "hrsh7th/cmp-path"         -- Path Completions
-	use "hrsh7th/cmp-cmdline"      -- Cmdline Completions
-	use "hrsh7th/cmp-nvim-lsp"     -- LSP Completion
-	use "saadparwaiz1/cmp_luasnip" -- Snippet Completions
+  -- CMP
+  use "hrsh7th/nvim-cmp"         -- Completion plugin
+  use "hrsh7th/cmp-buffer"       -- Buffer Completions
+  use "hrsh7th/cmp-path"         -- Path Completions
+  use "hrsh7th/cmp-cmdline"      -- Cmdline Completions
+  use "hrsh7th/cmp-nvim-lsp"     -- LSP Completion
+  use "saadparwaiz1/cmp_luasnip" -- Snippet Completions
 
-	-- Snippets
-	use "L3MON4D3/LuaSnip"             -- Snippet Engine
-	use "rafamadriz/friendly-snippets" -- A Bunch of Snippets to use
+  -- Snippets
+  use "L3MON4D3/LuaSnip"             -- Snippet Engine
+  use "rafamadriz/friendly-snippets" -- A Bunch of Snippets to use
 
-	-- LSP
-	use "neovim/nvim-lspconfig"           -- LSP Enabler
-	use "williamboman/nvim-lsp-installer" -- LSP Installer
+  -- LSP
+  use "neovim/nvim-lspconfig"           -- LSP Enabler
+  use "williamboman/nvim-lsp-installer" -- LSP Installer
 
-	-- Treesitter
-	use {                                -- Main
-	"nvim-treesitter/nvim-treesitter", -- Treesitter
-	run = ":TSUpdate"                  -- package
-	}                                    -- --
-	use "p00f/nvim-ts-rainbow"           -- Rainbow parentheses
+  -- Treesitter
+  use {                                -- Main
+  "nvim-treesitter/nvim-treesitter", -- Treesitter
+  run = ":TSUpdate"                  -- package
+  }                                    -- --
+  use "p00f/nvim-ts-rainbow"           -- Rainbow parentheses
 
-	if PACKER_BOOTSTRAP then
-	  require("packer").sync()
-	end
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
